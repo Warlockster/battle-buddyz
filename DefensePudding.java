a0038a521d154144bf6d19906ff4a0f65b93b1d9
Warlockster <cinnamonkey103@gmail.com>
2017-12-31 18:46:02 -0800

    Update DefensePudding.java

--- DefensePudding.java
+++ DefensePudding.java
@@ -2,5 +2,14 @@ package com.company;
 
 public class DefensePudding extends StatPudding
 {
-
+    public DefensePudding(String name)
+    {
+        super(name, def, 1.25);
+    }
+    
+    public String toString()
+    {
+        return "Defense Pudding" + 
+               "\n\nDescription: Increase your current Battle Buddy's defense by 25% while it remains in play.";
+    }
 }
