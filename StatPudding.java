980bd6a647d82d069c490072f671ad82fe10a41d
Warlockster <cinnamonkey103@gmail.com>
2018-01-01 13:58:09 -0800

    Update StatPudding.java

--- StatPudding.java
+++ StatPudding.java
@@ -4,10 +4,12 @@ public class StatPudding extends Item
 {
     private String statToChange;
     private double statMultiplier;
+    private int owner;
+    private boolean isUsed;
 
-    public StatPudding(String name, String statToChange, double statMultiplier)
+    public StatPudding(String name, String statToChange, double statMultiplier, int owner, boolean isUsed)
     {
-        super(name);
+        super(name, owner, isUsed);
         this.statToChange = statToChange;
         this.statMultiplier = statMultiplier;
     }
