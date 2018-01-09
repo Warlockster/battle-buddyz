e05756568d89ea338df39120f479b7c606f413c0
Warlockster <cinnamonkey103@gmail.com>
2018-01-09 14:02:55 -0800

    added moves

--- Flaxon.java
+++ Flaxon.java
@@ -2,8 +2,14 @@ package com.company;
 
 public class Flaxon extends Buddy
 {
+    private Move[] flaxonMoves = new Move[4];
+    
     public Flaxon()
     {
-        super("Flaxon", 175, 5, 50, 5, new Move[4], "");
+        super("Flaxon", 175, 5, 50, 5, flaxonMoves, "");
+        flaxonMoves[0] = new DamageMove("Ray Gun", 90, 35, 1);
+        flaxonMoves[1] = new StatMove("Wrap", 95, "eva", ENEMY, 0.75);
+        flaxonMoves[2] = new DamageMove("Ray Gun Blast", 75, 55, 1);
+        flaxonMoves[3] = new StatMove("Acid Ink", 90, "def", ENEMY, 0.5);
     }
 }
